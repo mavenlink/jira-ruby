a31a8deceeef89b45ff7aaabfab6611043b76460
lhwalsh <lhw2096@yahoo.com>
2019-04-12 10:05:42 -0700

    Update User all method to use api v3 if the client is a cloud instance

--- lib/jira/resource/user.rb
+++ lib/jira/resource/user.rb
@@ -15,7 +15,8 @@ module JIRA
       # Cannot retrieve more than 1,000 users through the api, please see: https://jira.atlassian.com/browse/JRASERVER-65089
       def self.all(client)
         search_token = client.cloud_instance? ? "_" : "@"
-        response  = client.get("/rest/api/2/user/search?username=#{search_token}&maxResults=#{MAX_RESULTS}")
+        rest_base_path = client.cloud_instance? ? "/rest/api/3" : "/rest/api/2"
+        response  = client.get("#{rest_base_path}/user/search?username=#{search_token}&maxResults=#{MAX_RESULTS}")
         all_users = JSON.parse(response.body)
 
         all_users.flatten.uniq.map do |user|
