0db823eb4e4e35d3aa9ac1d3dede4517a67dbba5
aditiguptakantata <aditi.gupta@kantata.com>
2025-07-30 12:56:34 +0530

    clean up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,5 @@
 RSpec.configure do |config|
   config.before(:each) do
-    stub_request(:get, "http://foo:bar@localhost:2990/jira/rest/api/2/field")
-      .with(headers: {
-        'Accept'=>'application/json',
-        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'User-Agent'=>'Ruby'
-      })
-      .to_return(:status => 200, :body => '[{"id":"1","name":"Description","custom":false,"orderable":true,"navigable":true,"searchable":true,"clauseNames":["description"],"schema":{"type":"string","system":"description"}},{"id":"2","name":"Summary","custom":false,"orderable":true,"navigable":true,"searchable":true,"clauseNames":["summary"],"schema":{"type":"string","system":"summary"}}]', :headers => {})
-    stub_request(:get, "http://foo:bar@localhost:2990/jira/rest/api/2/project")
-      .with(headers: {
-        'Accept'=>'application/json',
-        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'User-Agent'=>'Ruby'
-      })
-      .to_return(:status => 200, :body => '', :headers => {})
-
     stub_request(:get, "http://foo:bar@localhost:2990/rest/api/3/search/jql?jql=project=%22SAMPLEPROJECT%22")
       .with(headers: {
         'Accept'=>'application/json',
@@ -31,14 +16,6 @@ RSpec.configure do |config|
         'User-Agent'=>/OAuth gem.*/
       })
       .to_return(:status => 200, :body => '{ "issues": [ {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {} ] }', :headers => {})
-
-    stub_request(:get, "http://foo:bar@localhost:2990/jira/rest/api/2/comment")
-      .with(headers: {
-        'Accept'=>'application/json',
-        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'User-Agent'=>'Ruby'
-      })
-      .to_return(:status => 200, :body => '{"comments":[{"self":"http://localhost:2990/jira/rest/api/2/issue/10002/comment/10000","id":"10000","body":"This is a comment. Creative."},{"self":"http://localhost:2990/jira/rest/api/2/issue/10002/comment/10001","id":"10001","body":"Another comment."}]}', :headers => {})
   end
 end
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
